afb5e52bdc03b28fb37c2c3c266d4c318753f970
Vinícius Lenci <vinicius.lenci@semeq.com>
2025-11-11 10:07:14 -0300

    feat: create login screen

--- app/src/main/java/com/example/mobilechallenge/MainActivity.kt
+++ app/src/main/java/com/example/mobilechallenge/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobilechallenge.ui.theme.MobileChallengeTheme
+import com.example.mobilechallenge.ui.views.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             MobileChallengeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    LoginScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileChallengeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/mobilechallenge/ui/views/LoginScreen.kt
@@ -0,0 +1,10 @@
+package com.example.mobilechallenge.ui.views
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+
+}
